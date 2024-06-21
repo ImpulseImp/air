c325ebed25a6d944a8cee6e5f95ba19124cbdb4e
Impulse <impulses4mini@gmail.com>
2024-06-21 22:58:26 +0300

    add components

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,15 @@
+import { Button } from '@/components/ui/button';
+import { buttonVariants } from '@/components/ui/button';
+import Link from 'next/link';
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <Button variant='outline'>Button</Button>
+      <Button asChild variant='link'>
+        <Link href='/login'>Login</Link>
+      </Button>
+    </div>
+  );
 }
 export default HomePage;
