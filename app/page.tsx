0ce0e71ef261db83ba66c36d8cd118f21d1865ef
Impulse <impulses4mini@gmail.com>
2024-06-21 23:25:05 +0300

    add profilePage

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,6 @@ function HomePage() {
   return (
     <div>
       <h1>HomePage</h1>
-      <Button variant='outline'>Button</Button>
-      <Button asChild variant='link'>
-        <Link href='/login'>Login</Link>
-      </Button>
     </div>
   );
 }
