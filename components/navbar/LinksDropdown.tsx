1653d9fb850b3dd9dda9359734ca0593d3648e13
Impulse <impulses4mini@gmail.com>
2024-06-30 17:16:42 +0300

    add icon

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -1,4 +1,20 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
+import { LuUser } from "react-icons/lu";
+import { Button } from "../ui/button";
+
 function LinksDropdown() {
-  return <div>LinksDropdown</div>;
+  return (
+    <Button size="icon">
+      <LuUser className="h-6 w-6 rounded-full bg-primary text-white" />
+    </Button>
+  );
 }
 export default LinksDropdown;
